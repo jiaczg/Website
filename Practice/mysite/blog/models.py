7341ecaa8859a39be134c449e107667314d67cd4
jiaeg <jiaczg@gmail.com>
2018-06-12 22:28:20 +0800

    新增阅读统计

--- Practice/mysite/blog/models.py
+++ Practice/mysite/blog/models.py
@@ -1,22 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class BlogType(models.Model):
-    type_name = models.CharField(max_length=15)
+    type_name = models.CharField(max_length=15, verbose_name='分类列表')
 
     def __str__(self):
         return self.type_name
 
+    class Meta:
+        verbose_name = '博客分类'
+        verbose_name_plural  = verbose_name
+
 class Blog(models.Model):
-    title = models.CharField(max_length=50)
-    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    created_time = models.DateTimeField(auto_now_add=True)
-    lsat_updated_time = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=50, verbose_name='文章标题')
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING, verbose_name='文章分类')
+    content = RichTextUploadingField()
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='作者')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    lsat_updated_time = models.DateTimeField(auto_now=True, verbose_name='修改时间')
 
     def __str__(self):
         return "<Blog: %s>" % self.title
 
     class Meta:
         ordering = ['-created_time']
+        verbose_name = '博客文章'
+        verbose_name_plural = verbose_name
+
+class ReadNum(models.Model):
+    read_num = models.IntegerField(default=0, verbose_name='阅读量')
+    blog = models.OneToOneField(Blog, on_delete=models.DO_NOTHING, verbose_name='文章标题')
+
+    class Meta:
+        verbose_name = '阅读统计'
+        verbose_name_plural = verbose_name
