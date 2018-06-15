285f407e404e3c2a6f60f877c0cfabf7e8715af9
jiaeg <jiaczg@gmail.com>
2018-06-15 23:59:11 +0800

    阅读优化3

--- Practice/mysite/blog/models.py
+++ Practice/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor_uploader.fields import RichTextUploadingField
+from read_statistics.models import ReadNumExpandMethod
 
 class BlogType(models.Model):
     type_name = models.CharField(max_length=15, verbose_name='分类列表')
@@ -12,7 +13,7 @@ class BlogType(models.Model):
         verbose_name = '博客分类'
         verbose_name_plural  = verbose_name
 
-class Blog(models.Model):
+class Blog(models.Model, ReadNumExpandMethod):
     title = models.CharField(max_length=50, verbose_name='文章标题')
     blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING, verbose_name='文章分类')
     content = RichTextUploadingField()
@@ -20,22 +21,10 @@ class Blog(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
     lsat_updated_time = models.DateTimeField(auto_now=True, verbose_name='修改时间')
 
-    def read_num(self):
-        return self.readnum.read_num
-        verbose_name = '阅读量'
-
     def __str__(self):
         return "<Blog: %s>" % self.title
 
     class Meta:
         ordering = ['-created_time']
         verbose_name = '博客文章'
-        verbose_name_plural = verbose_name
-
-class ReadNum(models.Model):
-    read_num = models.IntegerField(default=0, verbose_name='阅读量')
-    blog = models.OneToOneField(Blog, on_delete=models.DO_NOTHING, verbose_name='文章标题')
-
-    class Meta:
-        verbose_name = '阅读统计'
         verbose_name_plural = verbose_name
