b2c55f0e7882548a0dff14c319d80452fb758fd4
jiaeg <jiaczg@gmail.com>
2018-06-24 22:40:44 +0800

    登录注册

--- Practice/24/mysite/urls.py
+++ Practice/24/mysite/urls.py
@@ -29,5 +29,4 @@ urlpatterns = [
     path('register/', views.register, name='register'),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Practice/24/mysite/views.py
+++ Practice/24/mysite/views.py
@@ -7,38 +7,37 @@ from django.core.cache import cache
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.urls import reverse
-from read_statistics.utils import get_seven_days_read_date, get_today_hot_date, get_yesterday_hot_date
+
+from read_statistics.utils import get_seven_days_read_data, get_today_hot_data, get_yesterday_hot_data
 from blog.models import Blog
 from .forms import LoginForm, RegForm
 
 def get_7_days_hot_blogs():
     today = timezone.now().date()
     date = today - datetime.timedelta(days=7)
-    blogs = Blog.objects.filter(read_details__date__lt=today, read_details__date__gte=date) \
-                        .values('id', 'title') \
-                        .annotate(read_num_sum=Sum('read_details__read_num')) \
-                        .order_by('-read_num_sum')
+    blogs = Blog.objects \
+                .filter(read_details__date__lt=today, read_details__date__gte=date) \
+                .values('id', 'title') \
+                .annotate(read_num_sum=Sum('read_details__read_num')) \
+                .order_by('-read_num_sum')
     return blogs[:7]
 
 def home(request):
     blog_content_type = ContentType.objects.get_for_model(Blog)
-    dates, read_nums = get_seven_days_read_date(blog_content_type)
+    dates, read_nums = get_seven_days_read_data(blog_content_type)
 
-    # 获取七天热门博客的缓存数据
+    # 获取7天热门博客的缓存数据
     hot_blogs_for_7_days = cache.get('hot_blogs_for_7_days')
     if hot_blogs_for_7_days is None:
         hot_blogs_for_7_days = get_7_days_hot_blogs()
         cache.set('hot_blogs_for_7_days', hot_blogs_for_7_days, 3600)
-        print('cals')
-    else:
-    	print('use cache')
 
     context = {}
     context['dates'] = dates
     context['read_nums'] = read_nums
-    context['today_hot_date'] = get_today_hot_date(blog_content_type)
-    context['yesterday_hot_date'] = get_yesterday_hot_date(blog_content_type)
-    context['hot_blogs_for_7_days'] = get_7_days_hot_blogs()
+    context['today_hot_data'] = get_today_hot_data(blog_content_type)
+    context['yesterday_hot_data'] = get_yesterday_hot_data(blog_content_type)
+    context['hot_blogs_for_7_days'] = hot_blogs_for_7_days
     return render(request, 'home.html', context)
 
 def login(request):
@@ -75,3 +74,4 @@ def register(request):
     context = {}
     context['reg_form'] = reg_form
     return render(request, 'register.html', context)
+    
