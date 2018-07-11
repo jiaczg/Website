4b01328cac75476929b8404f6af05983e6b9e6a1
jiaeg <jiaczg@gmail.com>
2018-07-12 07:28:54 +0800

    清空

--- Practice/mysite/comment/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Comment
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-	list_display = ('id', 'content_object', 'text', 'comment_time', 'user', 'root', 'parent')
