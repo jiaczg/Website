54f72b57a5e3f7e79bef348076c4b69b7c90a03e
jiaeg <jiaczg@gmail.com>
2018-07-12 22:02:53 +0800

    Mysql数据库迁移

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,14 +83,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'root',
+        'PASSWORD': 'wujiacheng',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+# '''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
